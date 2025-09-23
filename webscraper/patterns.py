bd207f7072424ac8e51107ac3be22cb70b58d1fa
RoiJoleil <joig150301@gmail.com>
2025-09-23 10:42:37 +0200

    Removed Regex for contact

--- webscraper/patterns.py
+++ webscraper/patterns.py
@@ -14,36 +14,7 @@ email_pattern = re.compile(email_regex, re.VERBOSE)
 
 # ============= TELEHPONE =============
 telephone_regex = r'''
-    (?=[+0-9]) # must start with + or digit
-    [0-9\+\/\-\(\)\[\]\{\} \t]+ # number
+    (?=[+0-9])
+    [0-9\-\–\+\/\(\)\[\]\{\} \t]+
 '''
 telehpone_pattern = re.compile(telephone_regex, re.VERBOSE)
-
-
-# ============= CONTACT =============
-suchwort = [
-    "fr", "hr", "frau", "herr",
-    "Geschäftsführer", "GeschäftsführerIn", "Geschäftsführer/in", "Geschäftsführer:in",
-    "Inhaber", "Inhaberin",
-    "Vorsitzender", "Vorsitzende", "Vorstand",
-    "Leiter", "Leiterin",
-    "Ansprechpartner", "Ansprechperson",
-    "Redaktion", "Redaktionsteam",
-    "CEO", "Managing Director", "Owner",
-    "Director", "Head", "Contact Person",
-    "Editor", "Editorial Team"
-]
-escaped_suchwort = [re.escape(s) for s in suchwort]
-suchwort_pattern = "|".join(escaped_suchwort)
-contact_regex = rf'''
-    (?i)                # case-insensitive
-    \b
-    ({suchwort_pattern})[.:,;]?  # Suchwort, allow optional dot
-    \s+
-    ([A-ZÄÖÜ][a-zäöüß]*\.?)   # Vorname or initial
-    \s+
-    ([A-ZÄÖÜ][a-zäöüß]*\.?)   # Nachname
-'''
-contact_pattern = re.compile(contact_regex, re.VERBOSE)
-
-
